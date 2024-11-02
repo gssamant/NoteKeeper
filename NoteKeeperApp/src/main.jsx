c5c1b446f5cec2df7e5090596aaee95e00ed4b1b
GauravSinghSamant <thorusamant8@gmail.com>
2024-11-02 06:59:54 +0530

    removed StrictMode

--- NoteKeeperApp/src/main.jsx
+++ NoteKeeperApp/src/main.jsx
@@ -7,10 +7,10 @@ import { Provider } from 'react-redux'
 import {Toaster} from  'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <Provider store={store}>
       <App />
       <Toaster />
     </Provider>
-  </StrictMode>,
+  // </StrictMode>,
 )
