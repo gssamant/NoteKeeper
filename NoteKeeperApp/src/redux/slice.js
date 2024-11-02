ecbb615f0f0d04f3b3f496525adc25f543566100
GauravSinghSamant <thorusamant8@gmail.com>
2024-11-02 06:59:38 +0530

    syntax correction

--- NoteKeeperApp/src/redux/slice.js
+++ NoteKeeperApp/src/redux/slice.js
@@ -21,11 +21,11 @@ export const noteSlice = createSlice({
     },
 
     updateNote: (state, action) => {
-      const note = action.payload
+      const note = action.payload;
       const index = state.notes.findIndex((item) => item._id === note._id)
 
       if (index >= 0) {
-        state.notes(index) = note
+        state.notes[index] = note
         localStorage.setItem("notes", JSON.stringify(state.notes))
         toast.success("Note updated")
       }
